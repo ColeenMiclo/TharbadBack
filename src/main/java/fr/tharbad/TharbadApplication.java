2f466a53afbc7ad9af67f5811e2355cbbd52812a
Coleen Miclo <coleen.miclo@gmail.com>
2025-03-05 16:32:56 +0100

    first commit

---
+++ src/main/java/fr/tharbad/TharbadApplication.java
@@ -0,0 +1,13 @@
+package fr.tharbad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TharbadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TharbadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tharbad/TharbadApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tharbad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TharbadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
